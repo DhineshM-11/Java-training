d1fe81f62ac2bc381db20f36f302ba43aff93c39
siva_havoc24 <sivamurugavel24@gmail.com>
2025-08-05 18:54:59 +0530

    inserted

---
+++ Day9/list.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class list {
+    public static void main(String[] args) {
+        List<String>fruits=new LinkedList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        System.out.println(fruits);
+        System.out.println(fruits.get(2));
+        System.out.println(fruits.set(1, "grapes"));
+        System.out.println(fruits);
+        System.out.println(fruits.remove(0));
+
+    }
+}
