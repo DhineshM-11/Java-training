db96497412366d5cf8def3de70b3d7ec72b5e761
siva_havoc24 <sivamurugavel24@gmail.com>
2025-08-09 22:24:57 +0530

    inserted

--- Day15/Demo2.java
+++ Day15/Demo2.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Demo2 {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of x:");
+        int x=sc.nextInt();
+        System.out.print("Enter the value of y:");
+        int y=sc.nextInt();
+        int s=0;
+        for(int i=1;i<y+1;i++){
+            s=x*i;
+            System.out.println(s);
+        }
+    }
 }
